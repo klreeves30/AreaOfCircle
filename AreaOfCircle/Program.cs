c0fce86201fccb83cadceac76daba022d0255b62
klreeves30 <klreeves30@gmail.com>
2021-10-22 13:02:39 -0400

    initial commit

---
+++ AreaOfCircle/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AreaOfCircle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AreaOfCircle();
+        }
+
+        public static void AreaOfCircle()
+        {
+            
+            Console.WriteLine("What is the radius of a circle?");
+
+            var radius = double.Parse(Console.ReadLine());
+
+            var area = Math.PI * (radius * radius);
+
+             Console.WriteLine(area);
+        }
+    }
+}
+
